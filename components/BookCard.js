91c9aa58a2a333dba32202b6f85f80ebf2430e81
ursapictura <104770521+ursapictura@users.noreply.github.com>
2024-04-29 19:31:58 -0500

    Author name, published year, and genre added to all cards.

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
 import { addUserBook } from '../api/bookData';
 import { useAuth } from '../utils/context/authContext';
+import { getSingleAuthor } from '../api/authorData';
+import { getSingleGenre } from '../api/genreData';
 
 function BookCard({ bookObj, deleteBook, location }) {
-  // console.warn(bookObj);
+  const [author, setAuthor] = useState({});
+  const [genre, setGenre] = useState({});
 
+  useEffect(() => {
+    const getAuthor = async () => {
+      const authorData = await getSingleAuthor(bookObj.authorId);
+      setAuthor(authorData);
+    };
+
+    const getGenre = async () => {
+      const genreData = await getSingleGenre(bookObj.genreId);
+      setGenre(genreData);
+    };
+
+    getAuthor();
+    getGenre();
+  }, [bookObj]);
   const { user } = useAuth();
 
   const addThisBookToBookshelf = () => {
@@ -25,6 +42,13 @@ function BookCard({ bookObj, deleteBook, location }) {
       <Card.Img variant="top" src={bookObj.bookCover} alt={bookObj.title} style={{ height: '400px' }} />
       <Card.Body>
         <Card.Title>{bookObj.title}</Card.Title>
+
+        {author ? <h5>{author.name}</h5> : ''}
+
+        <h5>{bookObj.publishYear}</h5>
+
+        {genre ? <h5>Genre: {genre.genreName}</h5> : ''}
+
         <Link href={`/book/${bookObj.id}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
         </Link>
@@ -46,6 +70,9 @@ BookCard.propTypes = {
     bookCover: PropTypes.string,
     title: PropTypes.string,
     id: PropTypes.number,
+    publishYear: PropTypes.number,
+    authorId: PropTypes.number,
+    genreId: PropTypes.number,
   }).isRequired,
   deleteBook: PropTypes.func.isRequired,
   location: PropTypes.string.isRequired,
