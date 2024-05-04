283f4e4fdcbd6fef9c41f894323f2410e47e11c1
ursapictura <hsmithfreelance@gmail.com>
2024-05-04 13:56:34 -0500

    Authors and Books render in search. Need to change to search on individual pages

--- pages/search/[searchInput].js
+++ pages/search/[searchInput].js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { searchBooks } from '../../api/searchData';
-// import AuthorCard from '../../components/AuthorCard';
+import { searchBooks, searchAuthors } from '../../api/searchData';
+import AuthorCard from '../../components/AuthorCard';
 import BookCard from '../../components/BookCard';
 
 export default function Search() {
   const [filteredBooks, setFilteredBooks] = useState([]);
-  // const [filteredAuthors, setFilteredAuthors] = useState();
+  const [filteredAuthors, setFilteredAuthors] = useState([]);
   const router = useRouter();
   const { searchInput } = router.query;
   console.warn(searchInput);
@@ -15,24 +15,28 @@ export default function Search() {
     searchBooks(searchInput).then(setFilteredBooks);
   };
 
-  // const searchAllAuthors = () => {
-  //   searchAuthors(searchInput).then(setFilteredAuthors);
-  // };
+  const searchAllAuthors = () => {
+    searchAuthors(searchInput).then(setFilteredAuthors);
+  };
 
   useEffect(() => {
     searchAllBooks();
-    // searchAllAuthors();
+    searchAllAuthors();
   }, [searchInput]);
 
   return (
     <>
-      {/* <div className="d-flex flex-wrap">
-        {filteredAuthors.map((author) => (
-          <AuthorCard key={author.id} authorObj={author} />
-        ))}
-      </div> */}
+      {console.warn(filteredBooks[0])}
+      <h2 style={{ color: 'whitesmoke' }}>Author Results</h2>
       <div className="d-flex flex-wrap">
-        {filteredBooks.map((book) => <BookCard key={book.id} bookObj={book} onUpdate={searchAllBooks} />)}
+        {console.warn(filteredAuthors)}
+        {filteredAuthors.length > 1 ? filteredAuthors.map((author) => (
+          <AuthorCard key={author.id} authorObj={author} onUpdate={searchAllAuthors} />
+        )) : <h4 style={{ color: 'whitesmoke' }}>No Authors Found</h4>}
+      </div>
+      <h2 style={{ color: 'whitesmoke' }}>Book Results</h2>
+      <div className="d-flex flex-wrap">
+        {filteredBooks.length > 1 ? filteredBooks.map((book) => <BookCard key={book.id} bookObj={book} onUpdate={searchAllBooks} />) : <h4 style={{ color: 'whitesmoke' }}>No Books Found</h4> }
       </div>
     </>
   );
