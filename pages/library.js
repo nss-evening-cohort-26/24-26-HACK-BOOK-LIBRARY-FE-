ad39c71164740bf1822c077dc6a87a00baa0cbc1
Gregory Markus <gregmarkus1992@yahoo.com>
2024-05-09 20:41:47 -0500

    added a div to the library buttons and also changed their class name

--- pages/library.js
+++ pages/library.js
@@ -47,13 +47,14 @@ export default function Library() {
   return (
     <>
       <SearchBar location="library" />
-
-      <Button className="ratingSortButton" onClick={toggleSortType}>Sort by {sortType === 'title' ? 'Rating' : 'Title'}</Button>
-      {user.isAdmin && (
-      <Link href="/book/new/" passHref>
-        <Button className="addBookButton">Add Book</Button>
-      </Link>
-      )}
+      <div className="libraryButtonDiv">
+        <Button className="libraryButton" onClick={toggleSortType}>Sort by {sortType === 'title' ? 'Rating' : 'Title'}</Button>
+        {user.isAdmin && (
+        <Link href="/book/new/" passHref>
+          <Button className="libraryButton">Add Book</Button>
+        </Link>
+        )}
+      </div>
       <h4 className="text">Be quite in the god damn Library!</h4>
       <div className="d-flex flex-wrap">
         {books.map((book) => (
